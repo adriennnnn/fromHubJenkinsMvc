c6ddbaad417cb6852e26e042bfad3a19ebe333d6
adriennnnn <adrienthomascci@gmail.com>
2023-08-04 15:19:54 +0200

    test

--- src/main/java/fr/dawan/demojenkinmvc/DemoJenkinMvcApplication.java
+++ src/main/java/fr/dawan/demojenkinmvc/DemoJenkinMvcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoJenkinMvcApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJenkinMvcApplication.class, args);
 	}
