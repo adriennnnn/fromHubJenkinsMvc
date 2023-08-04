dc649e5ac745be229db04fd0585ad3ccf811d866
adriennnnn <adrienthomascci@gmail.com>
2023-08-04 11:28:28 +0200

    ini

---
+++ src/main/java/fr/dawan/demojenkinmvc/DemoJenkinMvcApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.demojenkinmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/demojenkinmvc/DemoJenkinMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.demojenkinmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
